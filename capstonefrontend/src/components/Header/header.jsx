439c4b0d3f96d75d2b74670631f8b22185e8d20e
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-28 11:08:09 -0500

    added basic feilds/content structure for the header component

--- capstonefrontend/src/components/Header/header.jsx
+++ capstonefrontend/src/components/Header/header.jsx
@@ -6,7 +6,23 @@ class Header extends Component {
      }
     render() { 
         return ( 
-            <div>test</div>
+            <div className='HeaderWrapper'>
+
+                <div className='HeaderLogo'>
+                    <h1>LeashLocal</h1>
+                </div>
+
+                <div className='HeaderSearch'>
+                    <form>
+                        <input type="text" name="searchbar" className='SearchBar' />
+                    </form>
+                </div>
+
+                <div className='HeaderAccount'>
+                    <h1>Account</h1>
+                    
+                </div>
+            </div>
          );
     }
 }
