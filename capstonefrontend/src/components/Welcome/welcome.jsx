8d68dc63d828ea7910f09cf7a08ce5526d171dc7
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-28 15:33:24 -0500

    added welcome card to homescreen and added some basic css

---
+++ capstonefrontend/src/components/Welcome/welcome.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './welcome.css';
+
+function Welcome(props){
+    return (
+            <div class='WelcomeBody'>
+            
+                <div>
+                    <h1 className='WelcomeMessage'>
+                        Welcome to Leash Local, <br/>
+                        Where your pet needs are just a click away../
+                    </h1>
+                    
+                </div>
+
+            </div>
+    );
+
+}
+ 
+export default Welcome;
--- capstonefrontend/src/components/app.jsx
+++ capstonefrontend/src/components/app.jsx
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import './app.css'
 import Header from './Header/header';
 import Navigation from './NavigationBar/navigation';
+import Welcome from './Welcome/welcome';
 
 class App extends Component {
     state = { }
     render() {
         return (
             <div className="MainWrapper">
-                
+
                 <div>
                     <Header/>
                 </div>
                 <div>
                     <Navigation/>
+                </div>
+                <div>
+                    <Welcome/>
                 </div>
 
             </div>
