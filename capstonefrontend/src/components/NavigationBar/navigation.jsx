a2b50a0a4b947c8b590a8eb96fd7895b7674b614
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-28 14:30:18 -0500

    added some basic css and content to the navigation component

---
+++ capstonefrontend/src/components/NavigationBar/navigation.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './navigation.css'
+
+
+
+function Navigation(props){
+    return (
+            <div class='NavigationBody'>
+                <div>
+                    <h3>Cats</h3>
+                </div>
+
+                <div>
+                    <h3>Dogs</h3>
+                </div>
+
+                <div>
+                    <h3>Reptiles</h3>
+                </div>
+
+                <div>
+                    <h3>Small Animals</h3>
+                </div>
+
+                <div>
+                    <h3>Birds</h3>
+                </div>
+
+                <div>
+                    <h3>Fish</h3>
+                </div>
+
+            </div>
+    );
+
+}
+ 
+export default Navigation;
--- capstonefrontend/src/components/app.jsx
+++ capstonefrontend/src/components/app.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './app.css'
 import Header from './Header/header';
+import Navigation from './NavigationBar/navigation';
 
 class App extends Component {
     state = { }
     render() {
         return (
             <div className="MainWrapper">
+                
                 <div>
                     <Header/>
+                </div>
+                <div>
+                    <Navigation/>
                 </div>
 
             </div>
